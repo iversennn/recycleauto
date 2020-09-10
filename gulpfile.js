675b67df38825b75d06e3d48fdc50f303e7b6a08
Simen Iversen <simen.m.iversen@gmail.com>
2020-09-10 21:51:26 +0200

    Second commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,41 @@ const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 function css() {
     return src('sass/**/*.scss')
       .pipe(sass())
       .pipe(minifyCSS())
-      .pipe(dest('css'))
+      .pipe(dest('dist/css'))
       .pipe(browserSync.stream())
+}
+
+function img(){
+  return src('media/*')
+  .pipe(imagemin())
+  .pipe(dest('dist/media'))
+  .pipe(browserSync.stream())
+}
+
+function html(){
+  return src('html/**/*.html')
+  .pipe(htmlmin())
+  .pipe(dest('dist'))
+  .pipe(browserSync.stream())
 }
 
 function watch(){
   browserSync.init({
     server: {
-      baseDir: './',
+      baseDir: 'dist/',
     }
   });
+  gulp.watch('./html/**/*.html', html);
+  gulp.watch('./media/**/*.jpg', img);
   gulp.watch('./sass/**/*.scss', css);
-  gulp.watch('./*.html').on('change', browserSync.reload)
+  gulp.watch('html/**/*.html').on('change', browserSync.reload);
 }
 
 exports.watch = watch;
